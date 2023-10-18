a4b8c7a7b4dd0d0012a049986b9bf3fb8dc497cb
Ana Antunes <125682413+Antunes2806@users.noreply.github.com>
2023-10-18 13:03:30 -0300

    consulta e agendamento
    Os dois ja estão linkada com o banco de dados

--- app.js
+++ app.js
@@ -7,12 +7,58 @@ const path = require('path');
 const consultasRouter = require('./consultas'); // Importe as rotas das consultas
 
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// Configurar a conexão com o banco de dados MySQL
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'phpmyadmin',
+    password: 'aluno',
+    database: 'consultas',
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+        throw err;
+    }
+    console.log('Conexão com o banco de dados MySQL estabelecida.');
+});
+
+// Configurar EJS como o motor de visualização
+app.set('view engine', 'ejs');
+
 
 // Use o roteamento para consultas
 app.use('/', consultasRouter);
 
 
+// Rota para exibir a página de agendamento de consultas
+app.get('/agendamento', (req, res) => {
+    res.render('pages/agendamento'); // Renderize a página de agendamento
+});
 
+// Rota para processar o agendamento de consultas
+app.post('/agendamento', (req, res) => {
+    const { paciente, especialidade, dor, dia } = req.body;
+
+    const query = 'INSERT INTO consultas (paciente, especialidade, dor, dia) VALUES (?, ?, ?, ?)';
+
+    db.query(query, [paciente, especialidade, dor, dia], (err, results) => {
+        console.log('${paciente}, ${especialidade} ${dor} ${dia}');
+        if (err) {
+            console.error('Erro na inserção de dados: ' + err.message);
+            return res.status(500).send('Erro na inserção de dados');
+        }
+
+        res.redirect('/consultas'); // Redirecione de volta para a página de consultas após a inserção
+    });
+});
+app.get('/consultas' ,(req, res) => {
+    res.sendFile(__dirname + '/consultas.html');
+})
 // Rota para exibir a página HTML
 app.get('/', (req, res) => {
 res.sendFile(__dirname + '/index.html');
@@ -31,9 +77,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // app.js
-
-
-app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
--- consultas.js
+++ consultas.js
@@ -4,38 +4,40 @@ const express = require('express');
 const router = express.Router();
 const db = require('./dbconfig');
 
-router.get('/consultas', (req, res) => {
-  const sql = 'SELECT nome, medico, motivo, data FROM consultas';
-
-  db.query(sql, (err, results) => {
-    if (err) {
-      console.error('Erro na consulta ao banco de dados: ' + err.message);
-      return res.status(500).send('Erro na consulta ao banco de dados');
-    }
-
-    res.render('consultas', { consultas: results });
-  });
+// Rota para exibir a página de agendamento
+router.get('/agendamento', (req, res) => {
+  res.render('agendamento'); // Renderize a página de agendamento
 });
 
-// Adicione uma rota para a submissão de dados do formulário
-router.post('/consultas', (req, res) => {
-  const { nome, medico, motivo, data } = req.body; 
-console.log(req.body)
-  const sql = 'INSERT INTO consultas (nome, medico, motivo, data) VALUES (?, ?, ?, ?)';
-  
-  db.query(sql, [nome, medico, motivo, data], (err, result) => {
+// Rota para processar o agendamento
+router.post('/agendamento', (req, res) => {
+  const { paciente, especialidade, dor, dia } = req.body;
+  console.log('Agendando consulta!');
+
+  const sql = 'INSERT INTO consultas (paciente, especialidade, dor, dia) VALUES (?, ?, ?, ?)';
+
+  db.query(sql, [paciente, especialidade, dor, dia], (err, result) => {
+    console.log('Enviando ao BD :Agendando consulta!:');
     if (err) {
       console.error('Erro na inserção de dados: ' + err.message);
       return res.status(500).send('Erro na inserção de dados');
     }
-    
+  });
+});
+// Adicione uma rota para a submissão de dados do formulário
+router.post('/consultas', (req, res) => {
+  const { paciente, especialidade, dor, dia } = req.body;
+
+  const sql = 'INSERT INTO consultas (paciente, especialidade, dor, dia) VALUES (?, ?, ?, ?)';
+
+  db.query(sql, [paciente, especialidade, dor, dia], (err, result) => {
+    if (err) {
+      console.error('Erro na inserção de dados: ' + err.message);
+      return res.status(500).send('Erro na inserção de dados');
+    }
+
     res.redirect('/consultas'); // Redirecione de volta para a página de consultas após a inserção
   });
 });
 
 module.exports = router;
-
-
-
-
- 
